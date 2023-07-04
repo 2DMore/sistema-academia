2945a1cfd6fae944fb9ae468ca5197ba7c9ec480
Daniel Moreno <A20216399@alumnos.uady.mx>
2023-07-04 15:27:48 -0600

    Configuracion sie

--- sie-api/src/main/java/edu/uday/sie/SieApiApplication.java
+++ sie-api/src/main/java/edu/uday/sie/SieApiApplication.java
@@ -2,8 +2,12 @@ package edu.uday.sie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@Configuration
+@PropertySource("application.properties")
 public class SieApiApplication {
 
 	public static void main(String[] args) {
